098f78fea297d439544bfab2241327e8023f110b
SalzsammlerPC <info@salzsammler.de>
2024-07-22 14:21:50 +0200

    feat: add rotation into driving direction

--- scripts/playerBycicle.gd
+++ scripts/playerBycicle.gd
@@ -10,5 +10,15 @@ func _physics_process(delta):
 		acceleration = 1
 	var direction = Input.get_vector("left", "right", "up", "down")
 	velocity = direction * speed * acceleration
+	rotation = lerp_angle(rotation, velocity.angle(), 0.25)
 
 	move_and_slide()
+
+
+func lerp_angle(from, to, weight):
+	return from + short_angle_dist(from, to) * weight
+
+func short_angle_dist(from, to):
+	var max_angle = PI * 2
+	var difference = fmod(to - from, max_angle)
+	return fmod(2 * difference, max_angle) - difference
--- scripts/playerCar.gd
+++ scripts/playerCar.gd
@@ -5,5 +5,15 @@ var speed : int = 400  # speed in pixels/sec
 func _physics_process(delta):
 	var direction = Input.get_vector("left", "right", "up", "down")
 	velocity = direction * speed
+	rotation = lerp_angle(rotation, velocity.angle(), 0.25)
 
 	move_and_slide()
+
+
+func lerp_angle(from, to, weight):
+	return from + short_angle_dist(from, to) * weight
+
+func short_angle_dist(from, to):
+	var max_angle = PI * 2
+	var difference = fmod(to - from, max_angle)
+	return fmod(2 * difference, max_angle) - difference
