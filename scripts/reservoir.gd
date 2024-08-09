3f2d043354fe524c4308624766f6f517a671979e
SalzsammlerPC <info@salzsammler.de>
2024-08-09 14:35:08 +0200

    feat: fix loading of resources and unloading

--- scripts/reservoir.gd
+++ scripts/reservoir.gd
@@ -4,10 +4,7 @@ var MAX_CAPACITY: int       = 100
 var currentCapacity: int    = 0
 var capacityVelocity: float = 0
 var isFilling: bool         = false
-var currentFillingBody
-
-signal _bike_score_gained
-signal _car_score_gained
+var isFull: bool            = true
 
 var BIKE_RESERVOIR_FILL_SCORE = 2
 var CAR_RESERVOIR_FILL_SCORE = 1
@@ -21,15 +18,17 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if isFilling:
-		if currentCapacity <= MAX_CAPACITY and currentCapacity >= -MAX_CAPACITY:
-			currentCapacity += capacityVelocity
-			
+		currentCapacity += capacityVelocity
+		clampf(currentCapacity, -MAX_CAPACITY, MAX_CAPACITY)
+		if currentCapacity < MAX_CAPACITY and currentCapacity > -MAX_CAPACITY:
+			isFull = false
 			#check if max is reached
-			if currentCapacity >= 100:
-				Score.add_bike_score() #Bike
-			if currentCapacity <= -100:
-				Score.add_car_score() #Car
-
+		if currentCapacity >= 100 and !isFull:
+			isFull = true
+			Score.add_bike_score() #Bike
+		if currentCapacity <= -100 and !isFull:
+			Score.add_car_score() #Car
+			isFull = true
 		#update progress bar depending on the current value
 		var sb = StyleBoxFlat.new()
 		progressBar.add_theme_stylebox_override("fill", sb)
@@ -40,8 +39,8 @@ func _process(delta):
 func _on_body_entered(body):
 	if body.name == "PlayerCar" or body.name == "PlayerBycicle":
 		isFilling = !isFilling
-		capacityVelocity = body.get_reservoir_filling_rate()
-		currentFillingBody = body
+		if isFilling:
+			capacityVelocity = body.get_reservoir_filling_rate()
 
 func _on_body_exited(body):
 	if body.name == "PlayerCar" or body.name == "PlayerBycicle":
