25e5290faa9f92c3efdce2025c9b8a481c844306
EnergyRacerLaptop <info@salzsammler.de>
2025-01-29 13:23:25 +0100

    feat: cleanup

--- scripts/reservoir.gd
+++ scripts/reservoir.gd
@@ -8,10 +8,6 @@ var isFull: bool            = false
 @onready var animation_player = $AnimationPlayer
 @onready var progressBar = $ProgressBar
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
