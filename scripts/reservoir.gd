7284a55d932a8470a7f6f8212f71c5ae2e6233db
Nic Schilling <niclas.schilling96@gmail.com>
2025-01-29 14:57:01 +0100

    fix: remove empty function

--- scripts/reservoir.gd
+++ scripts/reservoir.gd
@@ -12,11 +12,6 @@ var isFull: bool            = false
 var sound_played: bool  	= false
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass
-
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	if isFilling:
