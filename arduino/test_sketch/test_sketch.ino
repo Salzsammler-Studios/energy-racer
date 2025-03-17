24c4af986b9445e6413c04f1a2b7de8138460414
EnergyRacerLaptop <info@salzsammler.de>
2025-03-17 17:43:12 +0100

    feat: EVEN MORE HEATTTT

--- arduino/test_sketch/test_sketch.ino
+++ arduino/test_sketch/test_sketch.ino
@@ -6,7 +6,7 @@ int val;
 // Heat Plate variables
 int pressure = 0;
 int temperature = 1; //this is not in celsius, but I'll call it temperature anyway
-int temperatureRise = 30; //same
+int temperatureRise = 40; //same
 
 //Bike variables
 volatile long temp = 0;
